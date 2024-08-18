cbeef20fe94efa1f3a48b3325285f7d4b7f47b3b
Hazel <hazelfidecaro@gmail.com>
2024-08-18 14:05:40 +1200

    Update to 1.20.1

--- src/main/java/hipsterjazzbo/maccraft/mixin/WindowHintsMixin.java
+++ src/main/java/hipsterjazzbo/maccraft/mixin/WindowHintsMixin.java
@@ -1,21 +1,39 @@
 package hipsterjazzbo.maccraft.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.platform.Window;
+import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({Window.class})
 public abstract class WindowHintsMixin {
-    @Inject(
-            method = "<init>",
-            at = @At("RETURN")
-    )
-    private void adjustWindowHints(CallbackInfo callbackInfo) {
-        // Disable full resolution framebuffer on Retina displays,
-        // so that Minecraft can actually keep up.
-        GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
+    @Shadow
+    private int framebufferWidth;
+
+    @Shadow
+    private int framebufferHeight;
+
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 0), method = "<init>", remap = false)
+    private void onDefaultWindowHints(int hint, int value, Operation<Void> original) {
+        if (Minecraft.ON_OSX) {
+            original.call(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
+        }
+
+        original.call(hint, value);
+    }
+
+    @Inject(at = @At(value = "RETURN"), method = "refreshFramebufferSize")
+    private void afterUpdateFrameBufferSize(CallbackInfo ci) {
+        // prevents mis-scaled startup screen
+        if (Minecraft.ON_OSX) {
+            framebufferWidth /= 2;
+            framebufferHeight /= 2;
+        }
     }
 }
